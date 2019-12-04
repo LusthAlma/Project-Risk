664f2cd9e1747d486d23c62fa8b1a3e2daa83413
Lusth <tristan@image-et-net.com>
2019-12-04 12:10:13 +0100

    ajout de la partie serveur et message STOMP en suivant le tuto websocket, partie Angular pas encore opérationnel

---
+++ src/main/java/fr/alma/websocket/SimpleMessage.java
@@ -0,0 +1,23 @@
+package fr.alma.websocket;
+
+public class SimpleMessage {
+
+    private String name;
+
+    public SimpleMessage(){
+
+    }
+
+    public SimpleMessage(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+}
