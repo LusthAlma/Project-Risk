08366ea801f576278784e04d738daad6fec3549d
Roy Kevin <kevin-roy@hotmail.fr>
2019-12-09 15:13:49 +0100

    Add example of bean
    add an example of bean with all the repository where you can save
    Mission,Territoire,Continent at the begining of the execution.

--- src/main/java/fr/alma/risk/AccessingDataMySqlApplication.java
+++ src/main/java/fr/alma/risk/AccessingDataMySqlApplication.java
@@ -1,13 +1,34 @@
 package fr.alma.risk;
 
+import fr.alma.risk.accessingdatamysql.ContinentRepository;
+import fr.alma.risk.accessingdatamysql.MissionRepository;
+import fr.alma.risk.accessingdatamysql.TerritoireRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccessingDataMySqlApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(AccessingDataMySqlApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMySqlApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner demo(MissionRepository missionRepository, TerritoireRepository territoireRepository, ContinentRepository continentRepository) {
+        return (args) -> {
+            //example
+            missionRepository.save(new MissionConqueteTerritoire(12));
+            territoireRepository.save(new Territoire("Japon"));
+            continentRepository.save(new Continent("Afrique",8));
+            log.info("");
+        };
     }
 
 }
