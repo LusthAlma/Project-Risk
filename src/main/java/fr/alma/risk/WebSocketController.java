0f97f6e7828365aa297cc00bd7b7f8c90156888d
Lusth <tristan@image-et-net.com>
2019-12-13 12:09:14 +0100

    ajout du client Angular, résolution de tous les bugs (plus de problème cors, c'était lié à l'accés au server qui ne marchait pas), communication simple client/serveur via un chat

--- src/main/java/fr/alma/risk/WebSocketController.java
+++ src/main/java/fr/alma/risk/WebSocketController.java
@@ -1,13 +1,16 @@
 package fr.alma.risk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
-/*@CrossOrigin(origins = "http://localhost:4200")
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 @Controller
-@RequestMapping(path="/app")
 public class WebSocketController {
 
     private final SimpMessagingTemplate template;
@@ -17,25 +20,12 @@ public class WebSocketController {
         this.template=template;
     }
 
-    //@CrossOrigin(origins = "http://localhost:4200")
-    @MessageMapping("/connect")
-    @SendTo("/game-lobby")
+
+    @MessageMapping("/send/message")
     public void onReceivedMessage(String message) throws Exception {
         Thread.sleep(1000); // simulated delay
         this.template.convertAndSend("/game-lobby",new SimpleDateFormat("HH:mm:ss").format(new Date())+message);
     }
 
 }
-*/
-@Controller
-public class WebSocketController {
 
-
-    @MessageMapping("/hello")
-    @SendTo("/game-lobby/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-    }
-
-}
