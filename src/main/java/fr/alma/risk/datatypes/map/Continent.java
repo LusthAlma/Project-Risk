aefbf8438e116e3b22c5b150f5a82ad0e5d50c9f
Roy Kevin <kevin-roy@hotmail.fr>
2019-12-18 10:14:49 +0100

    Add method to Continent and add empty test class for it

--- src/main/java/fr/alma/risk/datatypes/map/Continent.java
+++ src/main/java/fr/alma/risk/datatypes/map/Continent.java
@@ -45,4 +45,40 @@ public class Continent {
         return renfortsBonus;
     }
 
+    /**
+     * Permet d'ajouter un territoire à la liste des territoires du continent
+     * @param territoire à ajouter
+     * @return renvoit vrai s'il n'était pas déja présent dans l'ensemble et qu'il a bien été ajouté.
+     */
+    public boolean addTerritoire(Territoire territoire){
+        return territoires.add(territoire);
+    }
+
+    /**
+     * Permet d'ajouter un ensemble de territoire à la liste des territoires du continent
+     * @param territoires à ajouter
+     * @return renvoit vrai s'ils n'étaient pas déja présent dans l'ensemble et s'ils ont bien été ajoutés.
+     */
+    public boolean addTerritoire(Set<Territoire> territoires){
+        return this.territoires.addAll(territoires);
+    }
+
+    /**
+     * Permet de retirer un territoire du continent
+     * @param territoire à retirer
+     * @return renvoit vrai s'il était présent et a donc été retiré
+     */
+    public boolean removeTerritoire(Territoire territoire){
+        return territoires.remove(territoire);
+    }
+
+    /**
+     * Permet de retirer un ensemble de territoire du continent
+     * @param territoires à retirer
+     * @return renvoit vrai s'ils étaient présent et ont donc été retirés
+     */
+    public boolean removeTerritoire(Set<Territoire> territoires){
+        return this.territoires.remove(territoires);
+    }
+
 }
---
+++ src/test/java/fr/alma/risk/datatypes/map/ContinentTest.java
@@ -0,0 +1,20 @@
+package fr.alma.risk.datatypes.map;
+
+import static org.junit.Assert.*;
+
+import fr.alma.risk.datatypes.map.Territoire;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContinentTest {
+
+    private Continent continentNamedTest;
+
+
+    @Before
+    public void setup(){
+
+    }
+
+
+}
